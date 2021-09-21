74135c93426d2bb72ddbe32f29139eb61a8644c9
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-21 12:04:08 +0200

    Added reinitialize method

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -15,8 +15,10 @@ class GameViewModel : ViewModel() {
         get() = _score
 
     private var _currentWordCount = 0
-    private lateinit var _currentScrambledWord: String
+    val currentWordCount: Int
+        get() = _currentWordCount
 
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
 
@@ -59,5 +61,27 @@ class GameViewModel : ViewModel() {
             getNextWord()
             true
         } else false
+    }
+
+    private fun increaseScore() {
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    /**
+     * Re-initializes the game data to restart the game.
+     */
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
     }
 }
